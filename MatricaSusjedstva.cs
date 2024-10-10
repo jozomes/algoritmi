0ba96f117c2357faa87f273a86476ab871b1b04a
jozomes <jozomestrovic0@gmail.com>
2024-10-10 14:12:59 +0200

    Add files via upload

---
+++ MatricaSusjedstva.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<List<int>> CreateAdjacencyList(List<List<int>> edges, int numVertices)
+    {
+        // Inicijalizirajte listu susjedstva
+        List<List<int>> adjList = new List<List<int>>(new List<int>[numVertices]);
+
+        // Popunite listu susjedstva na temelju bridova u
+        // grafu
+        for (int i = 0; i < edges.Count; i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            // Budući da je graf neusmjeren, stoga guramo bridove u oba smjera
+            adjList[u].Add(v);
+            adjList[v].Add(u);
+        }
+
+        return adjList;
+    }
+
+    static void Main()
+    {
+        // Neusmjereni graf od 4 čvora
+        int numVertices = 4;
+        List<List<int>> edges = new List<List<int>> { new List<int> { 0, 1 }, new List<int> { 0, 2 }, new List<int> { 1, 2 }, new List<int> { 2, 3 }, new List<int> { 3, 1 } };
+
+        // Kreirajte listu susjedstva
+        List<List<int>> listasus = CreateAdjacencyList(edges,numVertices);
+
+        // Ispišite listu susjedstva
+        foreach (List<int> item in listasus)
+        {
+            foreach (int item1 in item)
+            {
+                Console.Write(item1);
+            }
+        }
+        Console.ReadKey();
+    }
+}
---
+++ ProgramMATSUS.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<List<int>> CreateAdjacencyMatrix(List<List<int>> graph, int numVertices)
+    {
+        // Inicijaliziraj matricu susjedstva s nulama
+        List<List<int>> adjMatrix = new List<List<int>>(new List<int>[numVertices]);
+        for (int i = 0; i < numVertices; ++i)
+        {
+            adjMatrix[i] = new List<int>(new int[numVertices]);
+        }
+
+        // Popuni matricu susjedstva na temelju bridova u
+        // grafu
+        for (int i = 0; i < numVertices; ++i)
+        {
+            for (int j = 0; j < numVertices; ++j)
+            {
+                if (graph[i][j] == 1)
+                {
+                    adjMatrix[i][j] = 1;
+                    adjMatrix[j][i] = 1; // Za neusmjereni graf, postavi
+                                         // simetrične unose
+                }
+            }
+        }
+
+        return adjMatrix;
+    }
+
+    static void Main()
+    {
+        // Primjer grafa predstavljenog kao lista susjedstva
+        // Indeksi predstavljaju vrhove, a vrijednosti
+        // su liste susjednih vrhova
+        List<List<int>> graph = new List<List<int>> { new List<int> { 0, 1, 0, 0 } };
+        // unesi ostatak matrice
+  
+        int numVertices = graph.Count;
+
+
+        // Kreiraj matricu susjedstva
+        List<List<int>> matsus = CreateAdjacencyMatrix(graph,numVertices);
+
+        // Ispiši matricu susjedstva
+        Console.WriteLine("Matrica susjedstva:");
+        for (int i = 0; i < numVertices; ++i)
+        {
+            for (int j = 0; j < numVertices; ++j)
+            {
+                Console.Write(matsus[i][j] + " ");
+            }
+            Console.WriteLine(); //prelaazak u novu liniju nakon ispisa trenutnog
+        }
+    }
+}
+
